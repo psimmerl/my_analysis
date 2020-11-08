229ad6ebbc81ce2c0acc11c4166e2e9b705b1f6a
psimmerl <paul.simmerling@uconn.edu>
2020-11-07 23:31:34 -0500

    fixed dvz

--- my_root_job.sh
+++ my_root_job.sh
@@ -5,15 +5,15 @@ rm $dir/*.root
 #rm $dir/kaon_FD_events.hipo
 #run-groovy make_hipo.groovy /cache/clas12/rg-a/production/recon/fall2018/torus-1/pass1/v0/dst/train/skim4/*.hipo
 
-run-groovy gen_hist.groovy $dir/kaon_events.hipo $dir/no_cuts.root 
+run-groovy gen_hist.groovy $dir/kaon_FD_events.hipo $dir/no_cuts.root 
 python2.7 kaon/run_brandon_volatile.py $dir/no_cuts.root $dir/hists/
 
-run-groovy gen_hist.groovy $dir/kaon_events.hipo $dir/mass_cuts.root ecut pcut kpcut kmcut &
-run-groovy gen_hist.groovy $dir/kaon_events.hipo $dir/vz_cut.root    vzcut &
-run-groovy gen_hist.groovy $dir/kaon_events.hipo $dir/cp_cut.root    cpcut &
-run-groovy gen_hist.groovy $dir/kaon_events.hipo $dir/ckp_cut.root   ckpcut &
-run-groovy gen_hist.groovy $dir/kaon_events.hipo $dir/ckm_cut.root   ckmcut &
-run-groovy gen_hist.groovy $dir/kaon_events.hipo $dir/cop_cuts.root  cpcut ckpcut ckmcut &
+run-groovy gen_hist.groovy $dir/kaon_FD_events.hipo $dir/mass_cuts.root ecut pcut kpcut kmcut &
+run-groovy gen_hist.groovy $dir/kaon_FD_events.hipo $dir/vz_cut.root    vzcut &
+run-groovy gen_hist.groovy $dir/kaon_FD_events.hipo $dir/cp_cut.root    cpcut &
+run-groovy gen_hist.groovy $dir/kaon_FD_events.hipo $dir/ckp_cut.root   ckpcut &
+run-groovy gen_hist.groovy $dir/kaon_FD_events.hipo $dir/ckm_cut.root   ckmcut &
+run-groovy gen_hist.groovy $dir/kaon_FD_events.hipo $dir/cop_cuts.root  cpcut ckpcut ckmcut &
 
 wait
 
